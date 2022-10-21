e933f9b84ddfcb7b9d59fb098ea3ef119a9f6e7c
borgepl <pdeborger@gmail.com>
2022-10-21 10:05:33 +0200

    Extensions classes for services

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
-    public class ApplicationServiceExtensions
+    public static class ApplicationServiceExtensions
     {
-        
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options => {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
     }
 }
