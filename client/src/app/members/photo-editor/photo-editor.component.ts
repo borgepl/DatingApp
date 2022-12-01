fb5253162966c933e92e8df129f7729efc716bb5
borgepl <pdeborger@gmail.com>
2022-12-01 11:44:10 +0100

    photouploader / ng2-file-upload

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs';
 import { Member } from 'src/app/models/member';
+import { User } from 'src/app/models/user';
+import { AccountService } from 'src/app/services/account.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,10 +14,48 @@ import { Member } from 'src/app/models/member';
 export class PhotoEditorComponent implements OnInit {
 
   @Input() member: Member | undefined;
-  
-  constructor() { }
+
+  uploader:FileUploader;
+  hasBaseDropZoneOver:boolean = false;
+  baseUrl = environment.apiUrl;
+  user: User | undefined;
+  response:string;
+
+  constructor( private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => { if (user) this.user = user }
+    })
+  }
 
   ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
   }
 
 }
