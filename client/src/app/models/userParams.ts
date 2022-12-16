b919c9b3bddee8f33f3bf173128dad094f611737
borgepl <pdeborger@gmail.com>
2022-12-16 12:21:53 +0100

    Ordering in the client

--- client/src/app/models/userParams.ts
+++ client/src/app/models/userParams.ts
@@ -6,6 +6,7 @@ export class userParams {
   maxAge = 99;
   pageNumber = 1;
   pageSize = 5;
+  orderBy = 'lastActive';
 
   constructor(user: User) {
     this.gender = user.gender === 'female' ? 'male' : 'female';
