6503ca4af94f069059e64a242f69c63839229273
borgepl <pdeborger@gmail.com>
2023-01-03 16:28:40 +0100

    Updating the seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -7,29 +7,31 @@ using System.Text.Json;
 using API.Entities;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers( DataContext context) {
+        public static async Task SeedUsers(UserManager<AppUser> userManager) {
 
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
+               
                 user.UserName = user.UserName.ToLower();
+
+                // using var hmac = new HMACSHA512();
                 // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
                 // user.PasswordSalt = hmac.Key;
 
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd" );
 
             }
 
-            await context.SaveChangesAsync();
         }
     }
 }
