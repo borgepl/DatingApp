43c600ddd4e6ddc71055ca74899465c3947cdb31
borgepl <pdeborger@gmail.com>
2023-01-11 16:20:04 +0100

    Optimizing the presencehub

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -19,20 +19,23 @@ namespace API.SignalR
         }
         public override async Task OnConnectedAsync()
         {
-            await tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+            var isOnline = await tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+            if (isOnline)
+                await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
 
             var currentUsers = await tracker.GetOnlineUsers();
-            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            var isOffline = await tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
+            if (isOffline)
+                await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
 
-            var currentUsers = await tracker.GetOnlineUsers();
-            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+            // Removed from here -- Optimized 
+            // var currentUsers = await tracker.GetOnlineUsers();
+            // await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
