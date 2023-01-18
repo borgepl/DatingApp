eddeaa3191a6b7ec1b4a884c650e973815563a7b
borgepl <pdeborger@gmail.com>
2023-01-18 16:49:31 +0100

    Updated to .NET6 new features

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+    var builder = WebApplication.CreateBuilder(args);
 
-namespace API
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
+    // add services to the container.
+
+        // our own application services
+        builder.Services.AddApplicationServices(builder.Configuration);
+
+        // default services 
+        builder.Services.AddControllers();
+        builder.Services.AddCors();
+            
+        // our own identity services
+        builder.Services.AddIdentityServices(builder.Configuration);
+
+        // default Swagger config
+        builder.Services.AddSwaggerGen(c =>
         {
-            var host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatingAPI", Version = "v1" });
+        });
+
+    // configure the HTTP request pipeline.
+
+    var app = builder.Build();
+    var env = app.Environment;
+    
+        app.UseMiddleware<ExceptionMiddleware>();
+            
+        if (env.IsDevelopment())
+        {
+            // app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        //obsolete for net6 - app.UseRouting();
+
+        app.UseCors(x => x.AllowCredentials().AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("http://localhost:4200")
+            .WithOrigins("https://localhost:4200"));
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        // to host client angular app inside the API server.
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.MapControllers();
+        app.MapHub<PresenceHub>("hubs/presence");
+        app.MapHub<MessageHub>("hubs/message");
+        app.MapFallbackToController("Index", "FallBack");
+
+            using var scope = app.Services.CreateScope();
             var services = scope.ServiceProvider;
             try 
             {
@@ -36,16 +70,5 @@ namespace API
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during seed or migration");
             }
-            
-            await host.RunAsync();
 
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+            await app.RunAsync();
