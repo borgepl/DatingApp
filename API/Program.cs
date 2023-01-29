cf33155f8bcea1e95686d7ee5269e26492994998
borgepl <59479791+borgepl@users.noreply.github.com>
2023-01-29 17:44:54 +0100

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -46,6 +46,10 @@
         var pgHost = pgHostPort.Split(":")[0];
         var pgPort = pgHostPort.Split(":")[1];
        
+       ILogger logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+       logger.LogInformation("This is a testlog");
+       logger.LogInformation(pgPass);
+       
         connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
     } 
     
